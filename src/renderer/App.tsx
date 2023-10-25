2c6dbcfc258660274152ac72c24e83ed829ce900
leoleoleolei <leo.lei@globalpay.com>
2023-10-25 23:00:55 +0800

    feat: Init electron entry file and preload file

---
+++ src/renderer/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hello = () => {
+    return (
+        <div>
+            This is a poker for grooming session
+        </div>
+    )
+}
+
+export default Hello;
